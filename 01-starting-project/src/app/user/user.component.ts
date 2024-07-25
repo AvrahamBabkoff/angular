9d8aa004e44fba9071f5bf93a24e68305434901b
avrahamb <abraham.babkoff@elbitsystems.com>
2024-07-25 09:07:25 +0300

    signals 03

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,19 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
-  onSelectUser() {
-  }
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
 
+  onSelectUser() {}
 }
