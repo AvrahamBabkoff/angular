d0120120a5fd619817ad6940056e9ed2acfebfee
avrahamb <abraham.babkoff@elbitsystems.com>
2024-07-25 13:44:19 +0300

    ifs

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -7,18 +13,16 @@ import { Component, computed, EventEmitter, Input, input, Output, output } from 
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) user!: User;
 
   @Output() customSelect = new EventEmitter();
   // customSelect = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.customSelect.emit(this.id);
+    this.customSelect.emit(this.user.id);
   }
 }
