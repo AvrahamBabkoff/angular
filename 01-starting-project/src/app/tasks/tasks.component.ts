88ad6520cdd9b65834bd3ccfeaf2ec42eebcd5ec
avrahamb <abraham.babkoff@elbitsystems.com>
2024-07-28 08:37:55 +0300

    dialog

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,19 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-
+import { Component, EventEmitter, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
 
-  @Input({required: true}) userId!: string;
-  @Input({required: true}) name!: string;
 
-   tasks = [
+  isAddingTask = false;
+  tasks = [
     {
       id: 't1',
       userId: 'u1',
@@ -40,10 +41,18 @@ export class TasksComponent {
   ];
 
   get selectedUserTasks() {
-    return this.tasks.filter(t=> t.userId === this.userId)
+    return this.tasks.filter((t) => t.userId === this.userId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter(t => t.id !== id);
+    this.tasks = this.tasks.filter((t) => t.id !== id);
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
