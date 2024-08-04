360fdb8b0a1e3c24607096e7e29ef521ca821e7d
avrahamb <abraham.babkoff@elbitsystems.com>
2024-08-04 08:02:45 +0300

    template

--- section-06/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ section-06/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
 
+  onSubmit(titleInput: HTMLInputElement) {
+    //console.dir(titleInput);
+    const enteredTitle = titleInput.value;
+    console.log('ENTERED TITLE: ' + enteredTitle);
+  }
 }
